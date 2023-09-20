0739c7f9add5a7e825d9e0fe8380ecb78e89f216
RhysWong <rhyswong@amd.com>
2023-09-20 16:57:09 +0800

    DropDown

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import dcc
 import pandas as pd
 import plotly.graph_objects as go
 import dash
 from dash import dcc, html
+from dash.dependencies import Input, Output
 from functions import plotgraph
 
 
@@ -13,9 +15,32 @@ df = pd.read_csv("guardbandcore-6.csv")
 columns_to_plot = ["CPU0 CORES CORE1 Voltage"]
 colors = ["rgb(255, 0, 255)", "rgb(200, 51, 53)"]
 
-graph_list = plotgraph(columns_to_plot, colors, df)
+#graph_list = plotgraph(columns_to_plot, colors, df)
 
-app.layout = html.Div(graph_list)
+options = []
+for option in df.columns:
+    options.append(option)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph'), dcc.Dropdown(id='drop', options=options, value=df.columns[1])
+])
+
+@app.callback(Output('graph','figure'),
+              [Input('drop', 'value')])
+def update(selected_column):
+    filtered_df = df[selected_column]
+
+    trace = []
+    trace.append(go.Scatter(
+        x = df['Time Stamp'],
+        y = filtered_df,
+        mode='lines',
+        line = {'color' : 'rgb(255,0,255)'},
+        name = 'linegraph'
+    ))
+
+
+    return {'data' : trace,'layout':go.Layout(title='myplot')}
 
 if __name__ == "__main__":
     app.run_server()
